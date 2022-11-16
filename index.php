a2da881a95517315afb5504edc633d8a454d4be2
sokolzera <sokolowski.pawelx@gmail.com>
2022-11-16 11:39:33 +0100

    index2

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require('class/User.class.php');
+echo "<pre>";
 $db = new mysqli('localhost', 'root', '', 'loginForm');
-$user = new User("jkowalski", "tajneHasło");
-var_dump($user);
-
+$user = new User("dowalski","taj3neHasło");
+$user->login();
+if($user->isAuth()) {
+    echo "Użytkownik zalogowany poprawnie";
+} else {
+    echo "Błąd logowania";
+}
 ?>
