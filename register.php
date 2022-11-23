a865efdb2ba798f0b0b07e2ac4bdd3dabfeeb5c9
sokolzera <sokolowski.pawelx@gmail.com>
2022-11-23 12:32:18 +0100

    pokonamcie123

--- register.php
+++ register.php
@@ -1,12 +1,19 @@
 
     <?php 
-        if(isset($_REQUEST['login']) && isset($_REQUEST['password']) &&
-            isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
-            $db = new mysqli('localhost', 'root', '', 'loginForm');
-            require('./../class/User.class.php');
+        if(isset($_REQUEST['login']) && isset($_REQUEST['password']))  {
+            require_once('config.php');
+            require_once('class/User.class.php');
             $user = new User($_REQUEST['login'], $_REQUEST['password']);
-            $user->register();
-           
+            $user->setFirstName($_REQUEST['firstName']);
+            $user->setLastName($_REQUEST['lastName']); 
+            if($user->register()) {
+                $twig->display("message.html.twig", ['message' => "Zarejestrowano poprawnie"]);
+            } else {
+                $twig->display("message.html.twig", ['message' => "Błąd rejestracji użytkownika"]);
+            }
+        } else {
+            $twig->display("register.html.twig");
         }
+           
     ?>
     
