63ff2d89d22ed759212534dcae0cb8d2972371a1
sokolzera <sokolowski.pawelx@gmail.com>
2022-11-30 11:36:12 +0100

    xd123 30.11

--- index.php
+++ index.php
@@ -5,13 +5,36 @@ use Steampixel\Route;
 require_once ('config.php');
 require_once('class/user.class.php');
 
-Route::add('/' , function() {
-    echo "Strona główna";
+Route::add('/login' , function() {
+    global $twig;
+    $twig->display('login.html.twig');
 });
 
 Route::add('/login' , function() {
-    echo "Strona logowania";
-});
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        //jeżeli już podano dane do logowania
+        
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            //echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            //echo "Błędny login lub hasło";
+            $twig->display('message.html.twig', 
+                                ['message' => "Błędny login lub hasło"]);
+        }
+    } else {
+        //jeśli jeszcze nie podano danych
+        //wyświetl formularz logowania
+        $twig->display('login.html.twig');
+    }
+},  'post');
+  
+    
 
 Route::run('/loginform');
 
