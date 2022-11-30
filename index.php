bba4fc9b7b76ce148fba77eb7324cfe1c19bff79
sokolzera <sokolowski.pawelx@gmail.com>
2022-11-30 10:36:00 +0100

    dziala 3 30.11

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
+
+use Steampixel\Route;
+
 require_once ('vendor/autoload.php');
 require('class/user.class.php');
-$loader = new \Twig\Loader\ArrayLoader([
-    'index' => "Hello {{name}}!",
 
-]);
-$twig = new \Twig\Environment($loader);
-echo $twig->render('index', ['name' => 'Paweł']);
-$db = new mysqli('localhost', 'root', '', 'loginForm');
-$user = new User("dowalski","dowalski");
-$user->login();
-if($user->isAuth()) {
-    echo "Użytkownik zalogowany poprawnie";
-} else {
-    echo "Błąd logowania";
-}
-
+Route::add()
 
 ?>
