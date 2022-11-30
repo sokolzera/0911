a629e398520065b14b0cd39ac21d4cef59f999a2
sokolzera <sokolowski.pawelx@gmail.com>
2022-11-30 10:36:14 +0100

    4 30.11

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 
 use Steampixel\Route;
 
-require_once ('vendor/autoload.php');
-require('class/user.class.php');
+require_once ('config.php');
+require_once('class/user.class.php');
 
-Route::add()
+Route::add('/' , function() {
+    echo "Strona główna";
+});
+
+Route::add('/login' , function() {
+    echo "Strona logowania";
+});
+
+Route::run('/loginform');
 
 ?>
