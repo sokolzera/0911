35d32ef862c317309a2caa4e15e00b96e88a2f46
sokolzera <sokolowski.pawelx@gmail.com>
2022-12-14 10:36:55 +0100

    dziala11

--- class/user.class.php
+++ class/user.class.php
@@ -1,28 +1,49 @@
 <?php
 class User {
-    private $db;
     private int $id;
+    private mysqli $db;
     private string $login;
     private string $password;
     private string $firstName;
     private string $lastName;
 
-    public function __construct(string $login, string $password)
-    {
+    public function __construct(string $login, string $password) {
         $this->login = $login;
         $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
         global $db;
         $this->db = &$db;
     }
-    
-    public function isAuth() : bool {
-        if(isset($this->id) && $this->id !=null)
-            return true;
-        else
-            return false;
-
+    public function __serialize() : array {
+        return array(   
+                        'id' => $this->id,
+                        'login' => $this->login,
+                        'password' => $this->password,
+                        'firstName' => $this->firstName,
+                        'lastName' => $this->lastName,
+                    );
     }
-    public function login() {
+    public function __unserialize(array $data) {
+        $this->id = $data['id'];
+        $this->login = $data['login'];
+        $this->password = $data['password'];
+        $this->firstName = $data['firstName'];
+        $this->lastName = $data['lastName'];
+        global $db;
+        $this->db = &$db;
+    } 
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+    }
+
+    public function login() : bool {
         $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
         $preparedQuery = $this->db->prepare($query); 
         $preparedQuery->bind_param('s', $this->login);
@@ -43,21 +64,6 @@ class User {
             return false;
         }
     }
-        
-        
-    
-    public function logout() {
-        
-    }
-    public function register() {
-        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
-        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-        $preparedQuery = $this->db->prepare($query); 
-        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
-                                            $this->firstName, $this->lastName);
-        $result = $preparedQuery->execute();
-        return $result;
-    }
     public function setFirstName(string $firstName) {
         $this->firstName = $firstName;
     }
@@ -67,6 +73,14 @@ class User {
     public function getName() : string {
         return $this->firstName . " " . $this->lastName;
     }
-
+    public function save() : bool {
+        $q = "UPDATE user SET
+                firstName = ?,
+                lastName = ?
+                WHERE id = ?";
+        $preparedQuery = $this->db->prepare($q);
+        $preparedQuery->bind_param("ssi", $this->firstName, $this->lastName, $this->id);
+        return $preparedQuery->execute();
+    }
 }
 ?>
